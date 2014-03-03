0a92ab1cb72dff32cd94a2e5616751987718a6da
mlwmlw <520cutecat@gmail.com>
2014-03-04 00:26:40 +0800

    stylus

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,8 @@
 var gulp = require('gulp');
 var livereload = require('gulp-livereload');
 var lrserver = livereload();
-//var lr = require('tiny-lr')
-//var livereloadport = 35729;
-//var lrserver = lr();
+var stylus = require('gulp-stylus');
 var serverport = 8080;
-
-
 
 gulp.task('default', ['serve', 'watch']);
 
@@ -14,8 +10,13 @@ gulp.task('watch', function() {
 	gulp.watch('public/**').on('change', function(file) {
 		lrserver.changed(file.path);
 	});
+	gulp.watch('public/assets/stylus/*', ['stylus'])
 });
-
+gulp.task('stylus', function() {
+	return gulp.src('./public/assets/stylus/*.styl')
+		.pipe(stylus())
+		.pipe(gulp.dest('./public/assets/css'));
+});
 
 gulp.task('serve', function() {
 	var http = require('http');
