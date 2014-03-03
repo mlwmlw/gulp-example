f521ab8ec6cacf970e58a22f056b6287e534e935
mlwmlw <520cutecat@gmail.com>
2014-03-04 00:52:09 +0800

    package.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,32 @@ var gulp = require('gulp');
 var livereload = require('gulp-livereload');
 var lrserver = livereload();
 var stylus = require('gulp-stylus');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish')
 var serverport = 8080;
 
-gulp.task('default', ['serve', 'watch']);
+gulp.task('default', ['devel']);
+gulp.task('devel', ['serve', 'watch']);
+gulp.task('production', ['serve']);
 
 gulp.task('watch', function() {
 	gulp.watch('public/**').on('change', function(file) {
 		lrserver.changed(file.path);
 	});
 	gulp.watch('public/assets/stylus/*', ['stylus'])
+	gulp.watch('public/assets/js/*.js', ['lint'])
 });
+
 gulp.task('stylus', function() {
 	return gulp.src('./public/assets/stylus/*.styl')
 		.pipe(stylus())
 		.pipe(gulp.dest('./public/assets/css'));
+});
+
+gulp.task('lint', function() {
+	return gulp.src('./public/assets/js/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter(stylish));
 });
 
 gulp.task('serve', function() {
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,8 @@
 window.onload = function() {
 	console.log('hello world!');
-}
+	jshint();
+};
+function jshint() {
+	var $this = 'This is function';
+	console.log($this);
+};
